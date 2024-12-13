636de290a2221ddff5013d83be271013cba64a83
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2024-12-13 10:26:22 +0530

    int datatype

---
+++ 01.folder/3.n.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+    int age = 22;
+    char star = '*';
+    float pi = 3.14;
+    printf ("age is %d\n",age);
+    printf ("star looks like %c\n",star);
+    printf("pi value of %f",pi);
+
+
+    return 0;
+    }
+    
