49618514fd13fecc2c34c4d8b729cba05c6e6b2f
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2024-12-29 08:18:50 +0530

    operator

---
+++ notes/06.operator_precendence.c
@@ -0,0 +1,24 @@
+// ham ek expression likh skte h..jiske andr sare operators ho..to fir inme precedence kaise deside hota h..to iske liye ata h..
+
+// ((a+b-c+d*c)>(4*3*5)&&(4>2))
+
+
+/* OPERATOR PRECENDENCE
+Priority   operator
+1           !           
+2           *,/,%
+3           +,-
+4           <,<=,>,>=
+5           ==,!=
+6           &&
+7           ||
+8           =
+
+*/
+
+/* divide rule:-
+x%2= 0 (div)
+(not divied) if = 1,2,-1,-5 ,kch v aa gya to mtlb not a divided hoga.
+
+to yaha pe modular check karnge agr modular 0 return kare to divide hoga ,agr modular 0 return n kare to divide ni hoga..
+*/
