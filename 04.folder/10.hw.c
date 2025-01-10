70d7fce504674deeca3d3ef444c3c1d44f9ea240
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-01-10 12:04:43 +0530

    hw

---
+++ 04.folder/10.hw.c
@@ -0,0 +1,8 @@
+//HOMEWORK
+/*
+A) Write a program to print the average of 3 numbers.
+
+B) Write a program to check if given character is digit or not.
+
+C) Write a program to print the smallest no.
+*/
