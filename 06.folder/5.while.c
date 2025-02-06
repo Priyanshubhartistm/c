b7f0a0ecf0fba920714237d2db42640ecd2d98c6
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-06 11:36:19 +0530

    while

---
+++ 06.folder/5.while.c
@@ -0,0 +1,17 @@
+// While loop
+/*
+while(condition){
+// do something
+}
+*/
+
+#include<stdio.h>
+int main(){
+    int i=1;
+    while(i<=5){
+        printf("hello\n");
+        i++;
+    }
+    return 0;
+
+}
