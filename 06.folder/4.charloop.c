aac2a34f725eca14810c35a5de67e521f914b487
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-09 13:36:49 +0530

    ch

---
+++ 06.folder/4.charloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    for(char ch='a'; ch<='z'; ch++){
+        printf("%c\n" , ch);
+    }
+    return 0;
+}
+
+// iterator ko kise v data type ke sath use kr skte h..example:- int,float,char,double etc.
