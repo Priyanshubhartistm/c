c35ce6d6b0314a5d60827745e7144e220019b8af
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-13 12:58:17 +0530

    que

---
+++ 01.chapter.c/2que.c
@@ -0,0 +1,12 @@
+// write a program to calculate area of a circle.(radius is given)
+
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("enter radius");
+    scanf("%f", &radius);
+
+    printf("area is : %f",3.14*radius*radius );
+
+
+}
