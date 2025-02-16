5e4f4454716f8361ee4439db422c0a31cacd821d
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-17 00:55:19 +0530

    que

---
+++ 1.notes/11.continue.c
@@ -0,0 +1,9 @@
+// continue statement   = skip to next iteration
+/*
+break se ulta h..continue 
+>> break khta h..sara km rok do or aage badh jao.
+>> But CONTINUE, khta h..bs ave wala kam rokna h..aage wala km suru kr do.
+mtlb ave ki bari wali sari line hat jayegi or agli bari wali run ho jyegi.
+
+
+*/
---
+++ 1.notes/14.typeoffunction.c
@@ -0,0 +1,59 @@
+// 1. TYPE OF FUNCTION ARE 2 -:
+
+/*
+i) LIBRARY FUNCTION       = IT IS SPECIAL FUN.inbuilt(c k andr diff. libiraries hote h..jinke andr already kch functions pade huye h..un functions ke nm keyword h.unke fun. ke nam se hm dubara koye functions ni bana skte h.."unke kam v phle se hi fixed h..) IN C.
+
+** EXAMPLE                = printf,scanf
+
+ii) USER FUNCTION          = jinko hm hi declare krte h..(as programmer)
+or hm hi define krte h..
+
+** to coder hi isko declare\define\& use kr rhe hote h..
+
+** EXAMPLE                = namaste,hello ya kch v hm bana skte h..
+
+
+
+2. PASSING ARGUMENTS -: 
+
+i) PARAMETER               = jis value ko fun.(take krta)leta h..usko khte h..parameter.
+
+ii) RETURN VALUE           = jis value ko fun.opesh deta h..usko khte h..return value.
+
+can take a value & give some value.
+
+** function ko likhne k 3 tarika h..basically
+1. void printHello();
+this is easy way to write function.it is normal tarika.asm
+
+2. void printTable(int n);
+isko kch return to ni krna h.bs table print karna h..
+
+3. int sum(int a, intb);
+hmra ek return type ho jaise, sum ke case me hamne int ko ek return type liya,
+or y function 2 parameter le raha h..dono parameters ko lega inse ek sum value banyega jo (a+b) ho or isko kr dega return.
+
+or return kr rha h..to ise value ka type kya h..int isliya hamne apne function ke nm ke samne, int likha diya.
+
+Argument         v/s      Parameters
+
+values that              values in function 
+are passed in            declaration &
+function call            definition
+
+used to send             used to receive
+value                    value
+
+actual                   actual 
+parameter                parameter
+
+NOTE :-
+a. Function can only return one value at a time.
+
+b. Changes to parameters in function don't change the value in calling function.(bcz a copy of arguments is passed to the function)
+
+
+
+
+
+*/
