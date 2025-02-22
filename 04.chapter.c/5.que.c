d9ecb1795a60653756037a97ea2af37c4a7f841c
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-22 18:24:10 +0530

    for loop

---
+++ 04.chapter.c/5.que.c
@@ -0,0 +1,13 @@
+// Pratice Qs. 22
+// Print reverse of the table for a number n.
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. :");
+    scanf("%d" , &n);
+    for(int i=10; i>=1; i--) {
+    printf("%d\n" ,n*i);
+    }
+    return 0;
+}
---
+++ 04.chapter.c/6.qu.c
@@ -0,0 +1,17 @@
+// pratice qs14.
+// print the no.s from 0 to n,if n is given 
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter no. : ");
+    scanf("%d",&n);
+
+    int i=0;
+    while(i<=n) {
+        printf("%d\n" , i);
+        i++;
+    }
+return 0;
+
+}
