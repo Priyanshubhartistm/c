e026d4895cbbb22881c484cc2fd6805b834666f2
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-23 11:27:40 +0530

    hw

---
+++ 04.chapter.c/7.hw.c
@@ -0,0 +1,19 @@
+// HOMEWORK SET
+/*
+a.  Search on what a "nested loop" is & print this pattern using it.
+
+*****
+*****
+*****
+*****
+
+b.  Write a program to check if a no. is prime or not.
+
+c.  Write a program to print prime no. in a range.
+
+
+
+
+
+
+*/
