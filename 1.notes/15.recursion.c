15035b79063bd7a0e6f2731120592daea23fe775
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-23 11:26:53 +0530

    recursion

---
+++ 1.notes/15.recursion.c
@@ -0,0 +1,34 @@
+// 1. RECURSION = (when a fun. calls itself )jb koye function khud hi ko call alga de fir dubara se khud hi ko call laga de to that is called recursion.
+
+/*           
+>> programming ke andr jo km loop se ho skta h..o sb km recursion se ho skta h..&  jo km recursion se ho skta h..o sb km loop  se ho skta h..
+
+>> jisko loop se krne me bht sari line likhni padti h..pr recursion me 2-3 lins me ho jata h..
+
+>> same jisko recursion se krne me bht sari line likhni padti h..pr loop  me 2-3 lins me ho jata h..
+
+>> to dekhna padt h..kaha recursion use hoga kha loop(iterator).
+
+2. PROPERTIES OF RECURSION 
+
+A. sare km ek dusre se ho skte h(iterations\recursion)
+
+B. recursion ki help se tree me bht easily code 2-3 line me done ho jata h..
+
+C. recursion br br chl raha h..jis condition pe jake rukega usko (base case) khte h..
+
+
+D. recursion br br chlti hi rhegi iski khi v base case ni hogi to use condition kokhte h..(stack overflow).basically stack overflow means - memory are full and program are crash.
+
+isliya recursion me base case hona bht compulary h.
+
+if(n ==0) {
+        return 1;
+    }                 // that is base case
+
+
+
+
+
+
+*/
