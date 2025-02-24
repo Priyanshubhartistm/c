5746b217e910cd90e3435cb818e0cb7e3b105ad2
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-02-24 14:59:39 +0530

    recursion

---
+++ 05.chapter.c/11.recursion.c
@@ -0,0 +1,29 @@
+//RECURSION = (when a fun. calls itself )jb koye function khud hi ko call alga de fir dubara se khud hi ko call laga de to that is called recursion.
+
+/*
+>> recursion ko ab math ki help se samjhte h.
+
+i)    f(x)            = x^2
+      f(2)            = 
+
+ii)   f(f(x))         = x^2
+      f(f(2))         = f(4)^2      = 16
+
+iii)  f(f(f(x)))      = x^2
+      f(f(f(2)))      = f(f(4))     = f(16)^2   = 256
+
+
+1. NORMAL FUNCTION CALL            = jb normal function ek recursion function ko call lgti h. to y normal fun. kch kch km krke opesh se value main function ko de deta h..
+
+>> example:-   main ---->  f(x) ----> y(x) 
+or fir y f(x) ko de or main ko de ise trh se hmra normal function call hota h..
+
+
+2. RECURSION FUNCTION CALL         = pr jb v hm recursion ki bt krte h..n to iske andr kch special hota h..
+
+>> example:-   main ---->  f(x) ----> f(x) ----> f(x)
+
+y fun.khud hi call lagyega or y, f(x) kch return krke f(x) ko dega fir y, kch value return krke y main fun. ko deta h.
+
+br br khud hi ko call lagane wale functions ko recursive function khte h..and ise process ko (recursion)khte h..
+*/
