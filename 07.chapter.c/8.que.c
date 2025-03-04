ebf8c5f0c567c672c4e40ee74e216152e690df3d
Priyanshu Bharti <bhartipriyanshustm@gmail.com>
2025-03-04 17:59:36 +0530

    array

---
+++ 07.chapter.c/8.que.c
@@ -0,0 +1,31 @@
+// Pratice Qs.42
+/*
+write a function to count the number of number in an array.
+1|2|3|4|5| = 3 odd no. 
+
+
+
+
+
+
+*/
+
+# include <stdio.h>
+
+int countOdd(int arr[], int n);
+
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+    printf("%d", countOdd(arr, 6));
+
+    return 0;
+}
+int countOdd(int arr[], int n) {
+    int count = 0;
+    for(int i=0; i<n; i++) {
+        if(arr[i] % 2 != 0) {
+            count++;
+        }
+    }
+    return count;
+}
